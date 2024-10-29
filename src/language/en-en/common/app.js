ccec393f336b306ad63bc65e6b86d9aa42e976ae
{JetBrain} <108869575+JetBrain2056@users.noreply.github.com>
2024-10-29 13:35:34 +0300

    Add files via upload

---
+++ src/language/en-en/common/app.js
@@ -0,0 +1,13 @@
+let app = {
+    // Heading
+    desktop     : 'Desktop',
+    constants   : 'Constants',
+    references  : 'References',
+    documents   : 'Documents',
+    reports     : 'Reports',       
+    addElement  : 'Add an element',
+    editElement : 'Edit an element',
+    addGroup    : 'Add an group',
+    editGroup   : 'Edit an group'
+}
+module.exports = { app }
---
+++ src/language/en-en/common/column_left.js
@@ -0,0 +1,16 @@
+let column_left = {
+    //Title
+    $_title_1                   : 'Subsystems',
+    $_title_2                   : 'Modules',
+    $_title_3                   : 'Roles',
+    $_title_4                   : 'Constants',
+    $_title_5                   : 'References',
+    $_title_6                   : 'Documents',
+    $_title_7                   : 'Processing',
+    $_title_8                   : 'Registers',
+    $_title_9                   : 'Reports',
+    
+    $_text_configmenu           : 'Configurator'
+}
+
+module.exports =   { column_left }
